a2eb9173733c4d1839663c424e4821b8da30f367
Abdul Qadir <62835844+abdulqadir7391@users.noreply.github.com>
2024-09-09 19:06:50 +0000

    conversion and operation

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,3 +1,5 @@
+/*
+// ---------Conversion---------
 let score = "32ab"
 console.log(typeof score)
 console.log(typeof(score))
@@ -10,4 +12,42 @@ let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn)
 
+*/
+// -------Operations-----------
 
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 ="Hello";
+let str2 = " Abdul";
+
+let str3 = str1+str2;
+console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(2 + 3 * 5 % 3);
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+// let num1,num2,num3;
+// num1=num2=num3 = 4; //Its a bad way to initialize values
+// console.log(num1)
+
+let gameCounter = 100;
+console.log(gameCounter);
+console.log(++gameCounter); //prefix values
+console.log(gameCounter++); //postfix values
