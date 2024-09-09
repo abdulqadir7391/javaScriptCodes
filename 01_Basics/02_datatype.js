377bf61e707c5e02eb1c1ebb47ecf8eb13c6201a
Abdul Qadir <62835844+abdulqadir7391@users.noreply.github.com>
2024-09-09 18:22:06 +0000

    datatypes

--- 01_Basics/02_datatype.js
+++ 01_Basics/02_datatype.js
@@ -1,1 +1,20 @@
 "use strict" //treat all JS code as newer version of JS.
+//alert(3+3) //it is not work in node.js, its only work in browsers
+console.log(3+3) // Code readablity shoud be High.
+console.log("Abdul Qadir")
+
+let name = "Abdul" // String DataType
+let age = 26 // Numeric DataType
+let isLoggedIn = true //Boolean DataType
+let abc = null
+
+// BigInt //it is used for big size of numbers.
+// null //it is stand-alone value and also DataType
+// undefined 
+// Symbol => it is unique
+
+
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(isLoggedIn));
+console.log(typeof(abc));
